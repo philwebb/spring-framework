5c6d8d97ee3c3b97d1d675c16d282308a2baa96e
Phillip Webb <pwebb@vmware.com>
2013-03-06 16:56:26 -0800

    Make @Configuration classes thread-safe
    Refactor ConfigurationClassEnhancer so that BeanFactory instances are
    not held against CGLIB Callback objects. Enhanced @Configuration classes
    now use the BeanFactoryAware interface in order to obtain a BeanFactory.
    
    This change has the additional benefit that a static final field can
    now be used to hold all Callback instances.
    
    Issue: SPR-10307

--- spring-context/src/main/java/org/springframework/context/annotation/ConfigurationClassPostProcessor.java
+++ spring-context/src/main/java/org/springframework/context/annotation/ConfigurationClassPostProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -348,7 +348,7 @@ public class ConfigurationClassPostProcessor implements BeanDefinitionRegistryPo
 			// nothing to enhance -> return immediately
 			return;
 		}
-		ConfigurationClassEnhancer enhancer = new ConfigurationClassEnhancer(beanFactory);
+		ConfigurationClassEnhancer enhancer = new ConfigurationClassEnhancer();
 		for (Map.Entry<String, AbstractBeanDefinition> entry : configBeanDefs.entrySet()) {
 			AbstractBeanDefinition beanDef = entry.getValue();
 			try {
