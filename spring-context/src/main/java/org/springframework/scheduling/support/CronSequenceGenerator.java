93576af6224bb91a469aa8e70da7972cbddacca9
Phillip Webb <pwebb@pivotal.io>
2018-06-13 18:36:02 -0700

    Revert "Inner assignments"
    This reverts commit e4e040c627fd029374b84dce12f37f115bff5cbc.

--- spring-context/src/main/java/org/springframework/scheduling/support/CronSequenceGenerator.java
+++ spring-context/src/main/java/org/springframework/scheduling/support/CronSequenceGenerator.java
@@ -372,8 +372,7 @@ public class CronSequenceGenerator {
 			return result;
 		}
 		if (!field.contains("-")) {
-			result[0] = Integer.valueOf(field);
-			result[1] = result[0];
+			result[0] = result[1] = Integer.valueOf(field);
 		}
 		else {
 			String[] split = StringUtils.delimitedListToStringArray(field, "-");
