e13a8f8e0d2ea89e4c06a230c3b2d474a1f1f683
Phillip Webb <pwebb@pivotal.io>
2018-06-18 14:18:20 -0700

    ArrayTypeStyle

--- spring-web/src/test/java/org/springframework/mock/web/test/MockJspWriter.java
+++ spring-web/src/test/java/org/springframework/mock/web/test/MockJspWriter.java
@@ -114,7 +114,7 @@ public class MockJspWriter extends JspWriter {
 	}
 
 	@Override
-	public void write(char value[], int offset, int length) throws IOException {
+	public void write(char[] value, int offset, int length) throws IOException {
 		getTargetWriter().write(value, offset, length);
 	}
 
