19a14ef3e30cec6f0f22926db47bf64cdf32ad54
Phillip Webb <pwebb@pivotal.io>
2019-03-04 21:06:44 -0800

    Try to make bridge detection faster

--- spring-core/src/main/java/org/springframework/core/BridgeMethodResolver.java
+++ spring-core/src/main/java/org/springframework/core/BridgeMethodResolver.java
@@ -66,30 +66,34 @@ public final class BridgeMethodResolver {
 		}
 
 		// Gather all methods with matching name and parameter size.
-		List<Method> candidateMethods = new ArrayList<>();
+		List<Method> candidateMethods = null;
 		Method[] methods = ReflectionUtils.getAllDeclaredMethods(bridgeMethod.getDeclaringClass());
 		for (Method candidateMethod : methods) {
 			if (isBridgedCandidateFor(candidateMethod, bridgeMethod)) {
+				if (candidateMethods == null) {
+					candidateMethods = new ArrayList<>(methods.length);
+				}
 				candidateMethods.add(candidateMethod);
 			}
 		}
 
-		// Now perform simple quick check.
-		if (candidateMethods.size() == 1) {
-			return candidateMethods.get(0);
+		if (candidateMethods != null) {
+			// Perform simple quick check.
+			if (candidateMethods.size() == 1) {
+				return candidateMethods.get(0);
+			}
+
+			// Search for candidate match.
+			Method bridgedMethod = searchCandidates(candidateMethods, bridgeMethod);
+			if (bridgedMethod != null) {
+				// Bridged method found...
+				return bridgedMethod;
+			}
 		}
 
-		// Search for candidate match.
-		Method bridgedMethod = searchCandidates(candidateMethods, bridgeMethod);
-		if (bridgedMethod != null) {
-			// Bridged method found...
-			return bridgedMethod;
-		}
-		else {
-			// A bridge method was passed in but we couldn't find the bridged method.
-			// Let's proceed with the passed-in method and hope for the best...
-			return bridgeMethod;
-		}
+		// A bridge method was passed in but we couldn't find the bridged method.
+		// Let's proceed with the passed-in method and hope for the best...
+		return bridgeMethod;
 	}
 
 	/**
@@ -155,7 +159,7 @@ public final class BridgeMethodResolver {
 			return false;
 		}
 		for (int i = 0; i < candidateParameters.length; i++) {
-			ResolvableType genericParameter = ResolvableType.forMethodParameter(genericMethod, i, declaringClass);
+			ResolvableType genericParameter = ResolvableType.forMethodParameter(genericMethod, i, declaringClass, false);
 			Class<?> candidateParameter = candidateParameters[i];
 			if (candidateParameter.isArray()) {
 				// An array type: compare the component type.
