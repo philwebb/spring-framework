15a2a036b0116cbc0b90c1010751377b2a70df83
Stephane Nicoll <snicoll@vmware.com>
2022-02-04 15:37:23 +0100

    Add infrastructure to ease bean registration
    This commit adds an infrastructure used by code generators to ease
    bean registration. BeanDefinitionRegistrar offers a builder style API
    that provides a way to hide how injected elements are resolved at
    runtime.
    
    Each callback offers a callback variant that permits to throw
    checked exceptions so that the generated code does not have to worry
    about generating catch statements for those.

---
+++ spring-beans/src/main/java/org/springframework/beans/factory/generator/config/package-info.java
@@ -0,0 +1,10 @@
+
+/**
+ * Classes used in generated code to ease bean registration.
+ */
+@NonNullApi
+@NonNullFields
+package org.springframework.beans.factory.generator.config;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
