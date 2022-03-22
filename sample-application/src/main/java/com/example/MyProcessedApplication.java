7550970dfee3c1358ff19b7e398d30cd5aa67bb8
Phillip Webb <pwebb@vmware.com>
2022-03-21 22:10:23 -0700

    Hack

--- sample-application/src/main/java/com/example/MyProcessedApplication.java
+++ sample-application/src/main/java/com/example/MyProcessedApplication.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.support.DefaultListableBeanFactoryInitializer;
+import org.springframework.context.annotation.ContextAnnotationAutowireCandidateResolver;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.core.io.support.SpringFactoriesLoader;
 
@@ -13,6 +14,7 @@ public class MyProcessedApplication {
 	public static void main(String[] args) {
 		GenericApplicationContext context = new GenericApplicationContext();
 		DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) context.getBeanFactory();
+		beanFactory.setAutowireCandidateResolver(new ContextAnnotationAutowireCandidateResolver());
 		List<DefaultListableBeanFactoryInitializer> beanFactoryInitializers = SpringFactoriesLoader
 				.forNamedItem(BeanFactory.class, "default").load(DefaultListableBeanFactoryInitializer.class);
 		beanFactoryInitializers.forEach(beanFactoryInitializer -> beanFactoryInitializer.initialize(beanFactory));
--- sample-application/src/main/java/com/example/MyService.java
+++ sample-application/src/main/java/com/example/MyService.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,14 +10,17 @@ public class MyService {
 
 	private final MyMetrics metrics;
 
-	public MyService(MyRepository repository, MyMetrics metrics) {
+	private final String value;
+
+	public MyService(MyRepository repository, MyMetrics metrics, @Value("{test}") String value) {
 		this.repository = repository;
 		this.metrics = metrics;
+		this.value = value;
 	}
 
 	@Override
 	public String toString() {
-		return "MyService using " + this.repository + " and " + this.metrics;
+		return "MyService using " + this.repository + " and " + this.metrics + " with " + this.value;
 	}
 
 }
