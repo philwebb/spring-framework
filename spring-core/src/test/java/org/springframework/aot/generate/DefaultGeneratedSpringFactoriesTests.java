f82e449c6d74435623f251f663c29b27c097a0aa
Phillip Webb <pwebb@vmware.com>
2022-04-13 18:15:55 -0700

    Add 'spring.factories' generation support classes
    Add `GeneratedSpringFactories` interface which can be used to
    generate a `spring.factories` file.

---
+++ spring-core/src/test/java/org/springframework/aot/generate/DefaultGeneratedSpringFactoriesTests.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2002-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.aot.generate;
+
+import java.nio.charset.StandardCharsets;
+import java.util.function.Function;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.aot.generate.GeneratedFiles.Kind;
+import org.springframework.aot.generate.GeneratedSpringFactories.Declarations;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.InputStreamSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link DefaultGeneratedSpringFactories}.
+ *
+ * @author Stephane Nicoll
+ * @author Phillip Webb
+ * @author Andy Wilkinson
+ */
+class DefaultGeneratedSpringFactoriesTests {
+
+	private static final String DEFAULT_CONTENT = """
+			com.example.FactoryA=\\
+			com.example.FactoryA1,\\
+			com.example.FactoryA2
+
+			com.example.FactoryB=\\
+			com.example.FactoryB1
+			""";
+
+	private static final String CUSTOM_CONTENT = """
+			com.example.FactoryD=\\
+			com.example.FactoryD1
+			""";
+
+	@Test
+	void writeToWritesContent() throws Exception {
+		DefaultGeneratedSpringFactories springFactories = new DefaultGeneratedSpringFactories();
+		Declarations defaultLocation = springFactories.forDefaultResourceLocation();
+		defaultLocation.add("com.example.FactoryA", "com.example.FactoryA1");
+		defaultLocation.add("com.example.FactoryA", "com.example.FactoryA2");
+		defaultLocation.add("com.example.FactoryB", "com.example.FactoryB1");
+		Declarations resourceLocation = springFactories.forResourceLocation("META-INF/custom.factories");
+		resourceLocation.add("com.example.FactoryD", "com.example.FactoryD1");
+		InMemoryGeneratedFiles generatedFiles = new InMemoryGeneratedFiles();
+		springFactories.writeTo(generatedFiles);
+		assertThat(generatedFiles.getGeneratedFileContent(Kind.RESOURCE, "META-INF/spring.factories"))
+				.isEqualTo(DEFAULT_CONTENT);
+		assertThat(generatedFiles.getGeneratedFileContent(Kind.RESOURCE, "META-INF/custom.factories"))
+				.isEqualTo(CUSTOM_CONTENT);
+	}
+
+	@Test
+	void writeToWhenHasExistingContentMerges() throws Exception {
+		Function<String, InputStreamSource> existing = name -> new ByteArrayResource(
+				"com.example.FactoryA = com.example.FactoryA1".getBytes(StandardCharsets.UTF_8));
+		DefaultGeneratedSpringFactories springFactories = new DefaultGeneratedSpringFactories(existing);
+		Declarations defaultLocation = springFactories.forDefaultResourceLocation();
+		defaultLocation.add("com.example.FactoryA", "com.example.FactoryA2");
+		defaultLocation.add("com.example.FactoryB", "com.example.FactoryB1");
+		InMemoryGeneratedFiles generatedFiles = new InMemoryGeneratedFiles();
+		springFactories.writeTo(generatedFiles);
+		assertThat(generatedFiles.getGeneratedFileContent(Kind.RESOURCE, "META-INF/spring.factories"))
+				.isEqualTo(DEFAULT_CONTENT);
+	}
+
+}
