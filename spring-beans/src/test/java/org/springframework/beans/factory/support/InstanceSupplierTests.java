b775a6e8c3681a93cb984daeda7f7aa1118f7bc8
Phillip Webb <pwebb@vmware.com>
2022-04-13 17:13:17 -0700

    Add InstanceSupplier and RegisteredBean support
    Add a new `InstanceSupplier` interface that can be used as an
    alternative to a regular bean `Supplier` when details about the
    bean being supplied are required to instantiate it. The new
    interface accepts a `RegisteredBean` instance which provides
    access to the bean name, the bean factory creating the bean
    and the bean definition.
    
    This interface is primarily designed to allow AOT generated code
    to autowire dependencies into the instance.

---
+++ spring-beans/src/test/java/org/springframework/beans/factory/support/InstanceSupplierTests.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2002-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.beans.factory.support;
+
+import java.util.function.Supplier;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.util.function.ThrowableSupplier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+
+/**
+ * Tests for {@link InstanceSupplier}.
+ *
+ * @author Stephane Nicoll
+ * @author Phillip Webb
+ * @author Andy Wilkinson
+ */
+class InstanceSupplierTests {
+
+	private RegisteredBean registeredBean = RegisteredBean.of(new DefaultListableBeanFactory(), "test");
+
+	@Test
+	void getWithoutRegisteredBeanThrowsException() {
+		InstanceSupplier<String> supplier = registeredBean -> "test";
+		assertThatIllegalStateException().isThrownBy(() -> supplier.get())
+				.withMessage("No RegisteredBean parameter provided");
+	}
+
+	@Test
+	void getWithExceptionWithoutRegisteredBeanThrowsException() {
+		InstanceSupplier<String> supplier = registeredBean -> "test";
+		assertThatIllegalStateException().isThrownBy(() -> supplier.getWithException())
+				.withMessage("No RegisteredBean parameter provided");
+	}
+
+	@Test
+	void getReturnsResult() throws Exception {
+		InstanceSupplier<String> supplier = registeredBean -> "test";
+		assertThat(supplier.get(this.registeredBean)).isEqualTo("test");
+	}
+
+	@Test
+	void ofSupplierWhenInstanceSupplierReturnsSameInstance() {
+		InstanceSupplier<String> supplier = registeredBean -> "test";
+		assertThat(InstanceSupplier.of(supplier)).isSameAs(supplier);
+	}
+
+	@Test
+	void usingSupplierAdaptsToInstanceSupplier() throws Exception {
+		InstanceSupplier<String> instanceSupplier = InstanceSupplier.using(() -> "test");
+		assertThat(instanceSupplier.get(this.registeredBean)).isEqualTo("test");
+	}
+
+	@Test
+	void ofInstanceSupplierAdaptsToInstanceSupplier() throws Exception {
+		InstanceSupplier<String> instanceSupplier = InstanceSupplier.of(registeredBean -> "test");
+		assertThat(instanceSupplier.get(this.registeredBean)).isEqualTo("test");
+	}
+
+	@Test
+	void getSuppliedInstanceWhenInstanceSupplierReturnsResult() throws Exception {
+		InstanceSupplier<String> supplier = registeredBean -> "test";
+		assertThat(InstanceSupplier.getSuppliedInstance(this.registeredBean, supplier)).isEqualTo("test");
+	}
+
+	@Test
+	void getSuppliedInstanceThrowableSupplierReturnsResult() throws Exception {
+		ThrowableSupplier<String> supplier = () -> "test";
+		assertThat(InstanceSupplier.getSuppliedInstance(this.registeredBean, supplier)).isEqualTo("test");
+	}
+
+	@Test
+	void getSuppliedInstanceThrowableSupplierWhenExceptionThrownDoesNotDoubleWrapException() {
+		RuntimeException ex = new RuntimeException();
+		ThrowableSupplier<String> supplier = () -> {
+			throw ex;
+		};
+		assertThatExceptionOfType(BeanCreationException.class)
+				.isThrownBy(() -> InstanceSupplier.getSuppliedInstance(this.registeredBean, supplier)).havingCause()
+				.isSameAs(ex);
+	}
+
+	@Test
+	void getSuppliedInstanceReturnsResult() throws Exception {
+		Supplier<String> supplier = () -> "test";
+		assertThat(InstanceSupplier.getSuppliedInstance(this.registeredBean, supplier)).isEqualTo("test");
+	}
+
+	@Test
+	void getSuppliedInstanceWhenThrowsExceptionThrowsBeanCreationException() {
+		RuntimeException ex = new RuntimeException();
+		InstanceSupplier<String> supplier = registeredBean -> {
+			throw ex;
+		};
+		assertThatExceptionOfType(BeanCreationException.class)
+				.isThrownBy(() -> InstanceSupplier.getSuppliedInstance(this.registeredBean, supplier)).havingCause()
+				.isSameAs(ex);
+
+	}
+
+	@Test
+	void callFromBeanFactoryProvidesRegisteredBean() {
+		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+		RootBeanDefinition beanDefinition = new RootBeanDefinition();
+		beanDefinition.setInstanceSupplier(
+				InstanceSupplier.of(registeredBean -> "I am bean " + registeredBean.getBeanName()));
+		beanFactory.registerBeanDefinition("test", beanDefinition);
+		assertThat(beanFactory.getBean("test")).isEqualTo("I am bean test");
+	}
+
+}
