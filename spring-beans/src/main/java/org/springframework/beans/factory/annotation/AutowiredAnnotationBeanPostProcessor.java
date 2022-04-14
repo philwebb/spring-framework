c61397741ccb92212c17eec4ce0a221ce5be1d38
Phillip Webb <pwebb@vmware.com>
2022-04-14 11:08:38 -0700

    Polish AutowiredAnnotationBeanPostProcessor

--- spring-beans/src/main/java/org/springframework/beans/factory/annotation/AutowiredAnnotationBeanPostProcessor.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/annotation/AutowiredAnnotationBeanPostProcessor.java
@@ -635,16 +635,13 @@ public class AutowiredAnnotationBeanPostProcessor implements SmartInstantiationA
 	 */
 	private class AutowiredFieldElement extends InjectionMetadata.InjectedElement {
 
-		private final boolean required;
-
 		private volatile boolean cached;
 
 		@Nullable
 		private volatile Object cachedFieldValue;
 
 		public AutowiredFieldElement(Field field, boolean required) {
-			super(field, null);
-			this.required = required;
+			super(field, null, required);
 		}
 
 		@Override
@@ -712,16 +709,13 @@ public class AutowiredAnnotationBeanPostProcessor implements SmartInstantiationA
 	 */
 	private class AutowiredMethodElement extends InjectionMetadata.InjectedElement {
 
-		private final boolean required;
-
 		private volatile boolean cached;
 
 		@Nullable
 		private volatile Object[] cachedMethodArguments;
 
 		public AutowiredMethodElement(Method method, boolean required, @Nullable PropertyDescriptor pd) {
-			super(method, pd);
-			this.required = required;
+			super(method, pd, required);
 		}
 
 		@Override
