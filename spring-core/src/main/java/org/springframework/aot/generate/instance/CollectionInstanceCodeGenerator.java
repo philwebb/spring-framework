18cb5e2a710a6fdce91c0b163e62e3264ffa5364
Phillip Webb <pwebb@vmware.com>
2022-04-13 17:36:25 -0700

    Add InstanceCodeGenerationService service and support classes
    Add `InstanceCodeGenerationService` which allows code to be
    generated based from an existing instance value. By default,
    the following types are supported:
    
     - Primitives and Wrappers
     - Strings
     - Arrays
     - Classes
     - Lists and Sets
     - Enums
     - Maps
     - ResolvableTypes
    
    The service can also be extended by adding custom
    `InstanceCodeGenerator` implementations.

---
+++ spring-core/src/main/java/org/springframework/aot/generate/instance/CollectionInstanceCodeGenerator.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2002-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.aot.generate.instance;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.springframework.core.ResolvableType;
+import org.springframework.javapoet.CodeBlock;
+import org.springframework.javapoet.CodeBlock.Builder;
+
+public class CollectionInstanceCodeGenerator<T extends Collection<?>> implements InstanceCodeGenerator {
+
+	private final Class<?> collectionType;
+
+	private final CodeBlock emptyResult;
+
+	public CollectionInstanceCodeGenerator(Class<?> collectionType, CodeBlock emptyResult) {
+		this.collectionType = collectionType;
+		this.emptyResult = emptyResult;
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public CodeBlock generateCode(Object value, ResolvableType type, InstanceCodeGenerationService service) {
+		if (this.collectionType.isInstance(value)) {
+			T collection = (T) value;
+			if (collection.isEmpty()) {
+				return this.emptyResult;
+			}
+			ResolvableType elementType = type.as(this.collectionType).getGeneric();
+			return generateCollectionCode(service, elementType, collection);
+		}
+		return null;
+	}
+
+	protected CodeBlock generateCollectionCode(InstanceCodeGenerationService service, ResolvableType elementType,
+			T collection) {
+		return generateCollectionOf(collection, this.collectionType, elementType, service);
+	}
+
+	protected final CodeBlock generateCollectionOf(Collection<?> collection, Class<?> collectionType,
+			ResolvableType elementType, InstanceCodeGenerationService service) {
+		Builder builder = CodeBlock.builder();
+		builder.add("$T.of(", collectionType);
+		Iterator<?> iterator = collection.iterator();
+		while (iterator.hasNext()) {
+			Object element = iterator.next();
+			CodeBlock elementCode = service.generateCode(element, elementType);
+			builder.add("$L", elementCode);
+			builder.add((!iterator.hasNext()) ? "" : ", ");
+		}
+		builder.add(")");
+		return builder.build();
+	}
+
+}
