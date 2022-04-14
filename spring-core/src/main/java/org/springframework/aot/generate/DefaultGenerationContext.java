03ad16bd5fae3bb6d21108887f7f63dc3d748b97
Phillip Webb <pwebb@vmware.com>
2022-04-13 18:16:14 -0700

    Add generation context interface
    Add `GenerationContext` interface and `DefaultGenerationContext`
    implementation as the central entry point for AOT processing.

---
+++ spring-core/src/main/java/org/springframework/aot/generate/DefaultGenerationContext.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2002-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.aot.generate;
+
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.util.Assert;
+
+/**
+ * Default implementation of {@link GenerationContext}.
+ *
+ * @author Stephane Nicoll
+ * @author Phillip Webb
+ * @author Andy Wilkinson
+ * @since 6.0
+ */
+public class DefaultGenerationContext implements GenerationContext {
+
+	private final ClassNameGenerator classNameGenerator;
+
+	private final GeneratedFiles generatedFiles;
+
+	private final GeneratedSpringFactories generatedSpringFactories;
+
+	private final RuntimeHints runtimeHints;
+
+	/**
+	 * Create a new {@link DefaultGenerationContext} instance backed by the specified
+	 * {@code generatedFiles}.
+	 * @param generatedFiles the generated files
+	 */
+	public DefaultGenerationContext(GeneratedFiles generatedFiles) {
+		this(new ClassNameGenerator(), generatedFiles, new DefaultGeneratedSpringFactories(), new RuntimeHints());
+	}
+
+	/**
+	 * Create a new {@link DefaultGenerationContext} instance backed by the specified
+	 * items.
+	 * @param classNameGenerator the class name generator
+	 * @param generatedFiles the generated files
+	 * @param generatedSpringFactories the generated spring factories
+	 * @param runtimeHints the runtime hints
+	 */
+	public DefaultGenerationContext(ClassNameGenerator classNameGenerator, GeneratedFiles generatedFiles,
+			GeneratedSpringFactories generatedSpringFactories, RuntimeHints runtimeHints) {
+		Assert.notNull(classNameGenerator, "'classNameGenerator' must not be null");
+		Assert.notNull(generatedFiles, "'generatedFiles' must not be null");
+		Assert.notNull(generatedSpringFactories, "'generatedSpringFactories' must not be null");
+		Assert.notNull(runtimeHints, "'runtimeHints' must not be null");
+		this.classNameGenerator = classNameGenerator;
+		this.generatedFiles = generatedFiles;
+		this.generatedSpringFactories = generatedSpringFactories;
+		this.runtimeHints = runtimeHints;
+	}
+
+	@Override
+	public ClassNameGenerator getClassNameGenerator() {
+		return this.classNameGenerator;
+	}
+
+	@Override
+	public GeneratedFiles getGeneratedFiles() {
+		return this.generatedFiles;
+	}
+
+	@Override
+	public GeneratedSpringFactories getGeneratedSpringFactories() {
+		return this.generatedSpringFactories;
+	}
+
+	@Override
+	public RuntimeHints getRuntimeHints() {
+		return this.runtimeHints;
+	}
+
+}
---
+++ spring-core/src/main/java/org/springframework/aot/generate/GenerationContext.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2002-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.aot.generate;
+
+import org.springframework.aot.hint.JavaSerializationHints;
+import org.springframework.aot.hint.ProxyHints;
+import org.springframework.aot.hint.ReflectionHints;
+import org.springframework.aot.hint.ResourceHints;
+import org.springframework.aot.hint.RuntimeHints;
+
+/**
+ * Central interface used for code generation.
+ * <p>
+ * An generation context provides:
+ * <ul>
+ * <li>Support for {@link #getClassNameGenerator() class name generation}.</li>
+ * <li>Central management of all {@link #getGeneratedFiles() generated files}.</li>
+ * <li>Registration of {@link #getGeneratedSpringFactories() spring.factories} content.
+ * <li>Support for the recording of {@link #getRuntimeHints() runtime hints}.</li>
+ * </ul>
+ *
+ * @author Stephane Nicoll
+ * @author Phillip Webb
+ * @author Andy Wilkinson
+ * @since 6.0
+ */
+public interface GenerationContext {
+
+	/**
+	 * Return the {@link ClassNameGenerator} being used by the context. Allows new class
+	 * names to be generated before they are added to the {@link #getGeneratedFiles()
+	 * generated files}.
+	 * @return the class name generator
+	 */
+	ClassNameGenerator getClassNameGenerator();
+
+	/**
+	 * Return the {@link GeneratedFiles} being used by the context. Used to write
+	 * resource, java source or class bytecode files.
+	 * @return the generated files
+	 */
+	GeneratedFiles getGeneratedFiles();
+
+	/**
+	 * Return the {@link GeneratedSpringFactories} files that will be written once AOT
+	 * processing has completed. Typically used to register generated code as a service so
+	 * that it will be applied when the AOT optimized application is run.
+	 * @return the spring factories
+	 */
+	GeneratedSpringFactories getGeneratedSpringFactories();
+
+	/**
+	 * Return the {@link RuntimeHints} being used by the context. Used to record
+	 * {@link ReflectionHints reflection}, {@link ResourceHints resource},
+	 * {@link JavaSerializationHints serialization} and {@link ProxyHints proxy} hints so
+	 * that the application can run as a native image.
+	 * @return the runtime hints
+	 */
+	RuntimeHints getRuntimeHints();
+
+}
