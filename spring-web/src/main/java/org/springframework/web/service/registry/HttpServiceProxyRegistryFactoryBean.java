1eafad17b1cec8b6aa8cae956addac84d2cf326d
Phillip Webb <phil.webb@broadcom.com>
2025-03-26 11:50:06 -0700

    Use ordered configurer stream

--- spring-web/src/main/java/org/springframework/web/service/registry/HttpServiceProxyRegistryFactoryBean.java
+++ spring-web/src/main/java/org/springframework/web/service/registry/HttpServiceProxyRegistryFactoryBean.java
@@ -17,7 +17,6 @@
 package org.springframework.web.service.registry;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -111,14 +110,9 @@ public final class HttpServiceProxyRegistryFactoryBean
 							groupAdapter)));
 
 		// Apply group configurers
-		this.groupAdapters.forEach((clientType, groupAdapter) -> {
-			Collection<? extends HttpServiceGroupConfigurer<?>> configurers =
-					this.applicationContext.getBeansOfType(groupAdapter.getConfigurerType()).values();
-
-			configurers.stream()
-					.filter(configurer -> groupAdapter.getConfigurerType().isInstance(configurer))
-					.forEach(configurer -> configurer.configureGroups(new DefaultGroups<>(clientType)));
-		});
+		this.groupAdapters.forEach((clientType, groupAdapter) ->
+			this.applicationContext.getBeanProvider(groupAdapter.getConfigurerType()).orderedStream()
+				.forEach(configurer -> configurer.configureGroups(new DefaultGroups<>(clientType))));
 
 		// Create proxies
 		Map<String, Map<Class<?>, Object>> groupProxyMap = this.groupSet.stream()
